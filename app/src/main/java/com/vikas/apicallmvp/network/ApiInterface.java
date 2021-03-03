ad8d5b5ac43eedecf154e6d5ab750cc974315d44
VikasSaini9565 <vikeykumar9565@gmail.com>
2021-03-03 09:06:11 +0530

    Initial commit

---
+++ app/src/main/java/com/vikas/apicallmvp/network/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.vikas.apicallmvp.network;
+
+import com.vikas.apicallmvp.model.MovieListResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+//    @GET("movie/popular")
+//    Call<MovieListResponse> getPopularMovies(@Query("api_key") String apiKey, @Query("page") int pageNo);
+@GET ("movie/popular")
+    Call<MovieListResponse> getPopularMovies(@Query("api_key") String apiKey, @Query("page") int pageNo);
+}
